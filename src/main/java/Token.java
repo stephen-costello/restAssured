14ce5876cd51b99dd37a000810d88ea2199605b5
Stephen Costello <115540183+stephen-costello@users.noreply.github.com>
2022-11-09 13:07:59 +0000

    Update Token.java

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -1,14 +1,5 @@
 import io.restassured.RestAssured;
 
-/**
- * Project Name    : rest-assured-demo
- * Developer       : Osanda Deshan
- * Version         : 1.0.0
- * Date            : 5/19/2022
- * Time            : 12:12 PM
- * Description     :
- **/
-
 public class Token {
 
     public static String getToken() {
